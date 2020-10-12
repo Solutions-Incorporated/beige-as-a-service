64456939e6aac7c36aaf9511b7cef91b8f76017b
danielchambers <dchambers@gocardless.com>
2020-10-12 16:19:31 +0100

    Sorted out Frontend callbacks

--- client/src/components/FileUploader.svelte
+++ client/src/components/FileUploader.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
-  let disabled = false;
   let submitted = false;
 	let promise = Promise.resolve([]);
   let files: string[] | Blob[];
+  
+  async function handleSubmit() {
+    promise = uploadFile()
+  }
 
   async function uploadFile() {
     if (files.length > 0) {
@@ -16,41 +19,53 @@
       })
       
       if (response.ok) {
-        return response.json();
+        const hexCode = await response.json()
+
+        const { scheme } = await mapColorScheme(hexCode);
+
+        const results = async () => await Promise.all(scheme.map(async (colorName) => {
+          const { color } = await findByName(colorName)
+
+          return { name: colorName, hexCode: color }
+        }))
+
+        const returnValue = await results().then(data => data)
+
+        return returnValue;
       } else {
         throw new Error("An error has occurred");
       }
     }
   }
 
-  async function handleSubmit() {
-    promise = uploadFile()
-    disabled = true
+  async function mapColorScheme(request) {
+    const response = await fetch("http://0.0.0.0:8000/color/scheme", {
+      method: "POST",
+      body: JSON.stringify(request)
+    })
+
+    if (response.ok) {
+        return await response.json();
+      } else {
+        throw new Error("An error has occurred");
+      }
+  }
+
+  async function findByName(colorName) {
+      const payload = JSON.stringify({name: colorName});
+      const response = await fetch("http://0.0.0.0:8000/color/name", {
+        method: "POST",
+        body: payload
+      })
+
+    if (response.ok) {
+        return await response.json();
+      } else {
+        throw new Error("An error has occurred");
+      }
   }
 </script>
-
-{#if submitted}
-  {#await promise}
-    <p>Doing some quik mafs</p>
-  {:then response}
-    <div class="color-name">
-      {response.color}
-    </div>
-
-    <div class="box" style="background-color:{response.color}"></div>
-  {:catch error}
-    <p style="color: red">{error.message}</p>
-  {/await}
-{/if}
-
-
-{#if !submitted}
-<form on:submit|preventDefault={handleSubmit} { disabled }>
-  <input required id="file" type="file" bind:files />
-  <input type="submit" value="Upload file" />
-</form>
-{/if}
-
+  
 <style>
     .box {
         width: 100px;
@@ -62,5 +77,44 @@
     .color-name {
       padding: 20px;
     }
+
+    .wrapper {
+      display: flex;
+    flex-direction: column;
+    }
+
+    .container {
+      display: inline-flex;
+      width: 40%;
+      justify-content: space-between;
+    }
 </style>
 
+{#if submitted}
+  {#await promise}
+    <p>Calculating all of the colors for you...</p>
+  {:then response}
+  <div class="container">
+    {#each response as color}
+    <div class="wrapper">
+      
+      <div class="color-name">
+        {color.name}
+      </div>
+      
+      <div class="box" style="background-color:{color.hexCode}"></div>
+    </div>
+    {/each}
+  </div>
+  {:catch error}
+    <p style="color: red">{error.message}</p>
+  {/await}
+{/if}
+
+
+{#if !submitted}
+<form on:submit|preventDefault={handleSubmit}>
+  <input required id="file" type="file" bind:files />
+  <input type="submit" value="Upload file" />
+</form>
+{/if}
